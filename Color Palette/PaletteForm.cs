7cc99d01e1281a64e2e9cf4a2b53b1939162d985
Alexey Larionov <alex_osoboe@mail.ru>
2019-04-14 17:43:40 +0300

    Palette is completed

--- Color Palette/PaletteForm.cs
+++ Color Palette/PaletteForm.cs
@@ -11,9 +11,36 @@ namespace ColorPalette
 {
     public partial class PaletteForm : Form
     {
+        ToolTip tooltip = new ToolTip();
+        Color currentColor;
         public PaletteForm()
         {
             InitializeComponent();
+            OnColorChanged(this, new EventArgs());
         }
+
+        private void OnColorChanged(object sender, EventArgs e)
+        {
+            // Изменение цвета индикатора при каждом изменении
+            int r = sliderRed.Value,
+                g = sliderGreen.Value,
+                b = sliderBlue.Value;
+            currentColor = Color.FromArgb(r, g, b);
+            pnlColorPreview.BackColor = currentColor;
+        }
+        private void OnColorChangeOver(object sender, EventArgs e)
+        {
+            // Шестнадцатиричные значения в буфере обмена и в подсказке
+            string hex = GetHexCode(currentColor);
+            Clipboard.SetText(hex);
+            tooltip.SetToolTip(pnlColorPreview, hex);
+        }
+
+        private string GetHexCode(Color c)
+        {
+            return "#" + c.R.ToString("X2") + c.G.ToString("X2") + c.B.ToString("X2");
+        }
+
+        
     }
 }
